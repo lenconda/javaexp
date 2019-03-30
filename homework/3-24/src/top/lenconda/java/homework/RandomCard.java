96f6eed5140178a1e6e133b859b17cc1e44e9ffb
lenconda <1053464288@qq.com>
2019-03-30 10:54:27 +0800

    finish homework/3-24

--- homework/3-24/src/top/lenconda/java/homework/RandomCard.java
+++ homework/3-24/src/top/lenconda/java/homework/RandomCard.java
@@ -8,7 +8,7 @@ class CardNumbers {
 	 * Get random number
 	 * @param {int} minimum
 	 * @param {int} maximum
-	 * @return {String}
+	 * @return
 	 */
 	private int getRandomNumber (int minimum, int maximum) {
 		return minimum + (int)(Math.random() * (maximum - minimum + 1));
@@ -29,10 +29,20 @@ class CardColors {
 	
 	private String[] colors = { "Clubs", "Diamonds", "Hearts", "Spades" };
 	
+	/**
+	 * Get random number
+	 * @param {int} minimum
+	 * @param {int} maximum
+	 * @return
+	 */
 	private int getRandomNumber (int minimum, int maximum) {
 		return minimum + (int)(Math.random() * (maximum - minimum + 1));
 	}
 	
+	/**
+	 * Get random card color
+	 * @return {String}
+	 */
 	public String random () {
 		int position = this.getRandomNumber(0, 3);
 		return this.colors[position];
@@ -43,7 +53,11 @@ class CardColors {
 public class RandomCard {
 	
 	public static void main (String[] args) {
-		String[] 
+		CardNumbers cardNumbers = new CardNumbers();
+		CardColors cardColors = new CardColors();
+		String number = cardNumbers.random();
+		String color = cardColors.random();
+		System.out.println("The card you picked is " + number + " of " + color);
 	}
 	
 }
